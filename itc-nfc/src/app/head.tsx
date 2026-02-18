06cdcb0d4ae6db565812e5579f9aa26cc75bcf85
Moe <moe.moepyesone77@gmail.com>
2026-02-18 20:49:21 +0800

    update code

---
+++ itc-nfc/src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+      <title>ITC Stamp Card</title>
+    </>
+  );
+}
